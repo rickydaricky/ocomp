9df7d0dfcf724b8a9b672984d3768d34e1b0114b
rickydaricky <zricky2000@gmail.com>
2021-10-28 00:59:54 -0400

    Final

---
+++ .history/src/main/database_20211027161141.py
@@ -0,0 +1,311 @@
+"""Program Modules: Regex, Firebase, Web Requests, and BeautifulSoup"""
+import re
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+import requests
+from bs4 import BeautifulSoup
+
+
+hero_list = ['ana', 'ashe', 'baptiste', 'bastion', 'brigitte', 'cassidy', 'dva', 'doomfist',
+             'echo', 'genji', 'hanzo', 'junkrat', 'lucio', 'mei', 'mercy', 'moira',
+             'orisa', 'pharah', 'reaper', 'reinhardt', 'roadhog', 'sigma', 'soldier76', 'sombra',
+             'symmetra', 'torbjorn', 'tracer', 'widowmaker', 'winston',
+             'wreckingball', 'zarya', 'zenyatta']
+
+cred = credentials.Certificate(
+    "/Users/rickydaricky/Desktop/ocomp/overbuff-compare-firebase-adminsdk-h8051-7f0b808aa7.json")
+firebase_admin.initialize_app(
+    cred, {'databaseURL': 'https://overbuff-compare-default-rtdb.firebaseio.com'})
+ref = db.reference("/Servers")
+
+try_header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
+}
+
+def dict_zip(*dicts):
+    """Zip equivalent for dictionaries, with padding option.
+    Based off an implementation from Skyrill here:
+    codereview.stackexchange.com/questions/160582/creating-zip-but-for-dictionaries
+    """
+    return {k: [d[k] for d in dicts] for k in dicts[0].keys()}
+
+async def set_user(self, guild_id, battle_id, nickname):
+    """Adds a player's battle_id to to their discord tag"""
+    nonactivity = await not_active(battle_id)
+    if nonactivity == 0:
+        print('Battle_ID not found')
+        return 0
+    if nonactivity == 1:
+        print('private profile')
+        return 1
+    print("active")
+    ref.child(guild_id).child('Users').update(
+        {nickname: {'battle_id': battle_id}})
+    return 2
+
+async def user_match(self, guild_id, nickname):
+    """matches an abbreviated nickname to a list of users in the database"""
+    snapshot = ref.child(guild_id).child('Users').get()
+    users_list = []
+    for key, _ in snapshot.items():
+        users_list.append(key)
+    return word_match(nickname, users_list)
+
+async def user_top_match(self, guild_id, nickname):
+    """matches an abbreviated nickname to a list of users in the database,
+    but if no user is found, searches within the top users list as well."""
+    try:
+        return user_match(self, guild_id, nickname)
+    except ValueError:
+        return 
+
+async def find_hero_data(self, guild_id, nickname, hero):
+    """Looks for a hero in the given player's stats page."""
+    battle_id = ''
+    snapshot = ref.child(guild_id).child('Users').get()
+    real_name = await Database.user_match(self, guild_id, nickname)
+    for key, val in snapshot.items():
+        if key == real_name:
+            battle_id = list(val.values())[0]
+    if battle_id == '':
+        raise ValueError('a user was not found in database')
+
+    return await crawl_search_hero(battle_id, word_match(hero, hero_list))
+
+
+async def find_hero(self, guild_id, nickname, hero):
+    """Returns data (in a string) for a player's stats on a specific hero"""
+    results = await Database.find_hero_data(self, guild_id, nickname, hero)
+    response = ''
+    for key, val in results.items():
+        response += '\n' + key + ': ' + val
+    return response
+
+@staticmethod
+async def refresh_top100():
+    """Crawls through the top 100 players on
+    overbuff and adds them to the database
+    """
+    top_url = 'https://www.overbuff.com/rankings'
+    header_request = requests.get(top_url, headers=try_header)
+    soup = BeautifulSoup(header_request.content, 'html5lib')
+    player_response_list = soup.find(
+        class_='table-condensed-mobile').contents[1]
+
+    for player in player_response_list:
+        player_url = player.contents[0].contents[0]['href']
+        player_id = player_url[12:]
+        top_ref = db.reference('/TopUsers')
+        top_ref.update({player_id: {'battle_id': player_id}})
+
+async def compare(guild_id, player_1, player_2, hero):
+    """Compares 2 players's overbuff stats on a particular hero"""
+    response = ''
+
+    p1_data = await Database.find_hero_data(guild_id, player_1, hero)
+    p2_data = await Database.find_hero_data(guild_id, player_2, hero)
+
+    spaces = ' ' * 15
+
+    try:
+        for key, val in Database.dict_zip(p1_data, p2_data).items():
+            val_1 = val[0]
+            val_2 = val[1]
+            space_gap = ' ' * \
+                max(0, round(1.8 * (25 - len(val_1) - len(key))))
+
+            # -1 means val_1 should be emphasized
+            # 0 means equal
+            # 1 means val_2 should be emphasized
+            status = 0
+
+            if 'Deaths' in key:
+                val_1_f = float(val_1)
+                val_2_f = float(val_2)
+                status = val_comp(val_2_f, val_1_f)
+            elif key == 'Record':
+                status = 0
+            elif key == 'Time Played':
+                val_1_vars = val_1.split(' ')
+                val_1_coef = val_1_vars[0]
+                val_1_unit = val_1_vars[1]
+                val_2_vars = val_2.split(' ')
+                val_2_coef = val_2_vars[0]
+                val_2_unit = val_2_vars[1]
+
+                if val_1_unit == val_2_unit:
+                    status = val_comp(val_1_coef, val_2_coef)
+                else:
+                    if val_1_unit == 'hours':
+                        status = -1
+                    elif val_2_unit == 'hours':
+                        status = 1
+
+            elif key == 'Hero Score ':
+                val_1_f = int(val_1.replace(',', ''))
+                val_2_f = int(val_2.replace(',', ''))
+                status = val_comp(val_1_f, val_2_f)
+            elif key == 'Hero Rank':
+                if val_1 == '∞' or val_2 == '∞':
+                    status = 0
+                elif '#' in val_1 and '#' in val_2:
+                    val_1_f = int(val_1.replace('#', ''))
+                    val_2_f = int(val_2.replace('#', ''))
+                    status = val_comp(val_2_f, val_1_f)
+                elif '%' in val_1 and '%' in val_2:
+                    val_1_f = int(val_1.replace('%', ''))
+                    val_2_f = int(val_2.replace('%', ''))
+                    status = val_comp(val_1_f, val_2_f)
+                else:
+                    if '#' in val_1:
+                        status = -1
+                    else:
+                        status = 1
+            elif key == 'Win Rate':
+                val_1_f = float(val_1[:-2])
+                val_2_f = float(val_2[:-2])
+                status = val_comp(val_1_f, val_2_f)
+            elif key == 'Final Blows' or key == 'Obj Kills' \
+                    or key == 'Eliminations' or key == 'Destruct Kills':
+                val_1_f = float(val_1)
+                val_2_f = float(val_2)
+                status = val_comp(val_1_f, val_2_f)
+            else:
+                status = val_comp(val_1, val_2)
+
+            if status == 0:
+                stat_status = f'\n{key}: {val_1}{space_gap}----{spaces}{key}: {val_2}'
+            elif status == -1:
+                stat_status = f'\n{key}: __**{val_1}**__{space_gap}----{spaces}{key}: {val_2}'
+            elif status == 1:
+                stat_status = f'\n{key}: {val_1}{space_gap}----{spaces}{key}: __**{val_2}**__'
+
+            response += stat_status
+        return response
+    except AttributeError as err:
+        raise ValueError(
+            'one of these players has not played this hero this season!') from err
+
+
+def val_comp(val_1, val_2) -> str:
+    """Compares two values and returns -1, 0, or 1
+    depending on whether val_1 is greater than,
+    equal to, or less than val_2"""
+    status = 0
+    if val_1 > val_2:
+        status = -1
+    elif val_1 < val_2:
+        status = 1
+    return status
+
+
+def word_match(word, word_list):
+    """Checks if any of the words in word_list start with the input word,
+    and if there is only one, return that full word.
+    Case insensitive.
+    """
+    possibles = []
+    for item in word_list:
+        if bool(re.match(word, item, re.I)):
+            possibles.append(item)
+    length_possibles = len(possibles)
+    if length_possibles != 1:
+        for item in possibles:
+            if word == item.lower() or word == item[:item.find('-')].lower():
+                return item
+        print(f'\n{word}, {possibles}')
+        raise ValueError(
+            'no user or hero could be found with one of these names, '
+            'and it does not appear to be an abbreviation either')
+    return possibles[0]
+
+
+def hero_word_match(hero):
+    """Runs word_match for a hero onto the
+    hero hero_list and parses it
+    """
+    lowercase_hero = word_match(hero, hero_list)
+    if lowercase_hero == 'wreckingball':
+        return 'Wrecking Ball'
+    elif lowercase_hero == 'soldier76':
+        return 'Soldier: 76'
+    elif lowercase_hero == 'dva':
+        return 'D.Va'
+    else:
+        return lowercase_hero.capitalize()
+
+
+
+
+async def search_prefix(guild_id):
+    """Returns the prefix for the current guild from the database"""
+    return ref.child(guild_id).child('Prefix').get()
+
+
+async def replace_prefix(guild_id, new_prefix):
+    """Updates the prefix for the current guild from the database
+    with the new prefix
+    """
+    ref.child(guild_id).child('Prefix').set(new_prefix)
+
+
+async def leave_guild(guild_id):
+    """Removes all guild data from the database"""
+    ref.child(guild_id).delete()
+
+
+def replace_last_char(battle_id, old_char, new_char):
+    """Replaces the last specified char in a string with a new char."""
+    split_battle_id = list(battle_id)
+    index = 0
+    hashtag_index = -1
+    for char in battle_id:
+        if char == old_char:
+            hashtag_index = index
+        index += 1
+    if hashtag_index == -1:
+        return battle_id
+
+    split_battle_id[hashtag_index] = new_char
+    return "".join(split_battle_id)
+
+
+def id_to_url(battle_id):
+    """Converts a battle id to a url based on that battle id"""
+    print(battle_id)
+    url = ('https://www.overbuff.com/players/pc/'
+           + replace_last_char(battle_id, '#', '-') + '?mode=competitive')
+    return url
+
+
+async def not_active(battle_id):
+    """Checks if a battle_id is real"""
+    overbuff_url = id_to_url(battle_id)
+    header_request = requests.get(overbuff_url, headers=try_header)
+    soup = BeautifulSoup(header_request.content, 'html5lib')
+    layout_error = soup.find_all(class_='layout-error')
+    private_error = soup.body.findAll(text='No QUICKPLAY Game Data Available')
+    if len(layout_error) > 0:
+        return 0
+    if len(private_error) > 0:
+        return 1
+    return 2
+
+
+async def crawl_search_hero(battle_id, hero):
+    """Searches a player's page for stats on one of their heroes"""
+    responses = {}
+    overbuff_url = id_to_url(battle_id)
+    header_request = requests.get(overbuff_url, headers=try_header)
+    soup = BeautifulSoup(header_request.content, 'html5lib')
+    hero_response_list = soup.findAll(class_='theme-hero-' + hero)
+    if not hero_response_list:
+        return
+    hero_response = hero_response_list[0]
+    padded_stats = hero_response.findAll(class_='stat')
+    for stat in padded_stats:
+        children = stat.contents
+        responses[children[len(children) - 1].text] = children[0].text
+    return responses
---
+++ .history/src/main/ocomp_20211027225221.py
@@ -0,0 +1,297 @@
+"""Modules imports: discord and database items"""
+from database import replace_last_char, search_prefix, \
+    replace_prefix, leave_guild, hero_word_match, \
+    refresh_top100, set_user, find_hero, user_top_match, \
+    database_compare
+from discord.ext import commands
+from discord.ext import tasks
+from discord.ext.commands import has_permissions, CheckFailure
+from discord_slash import SlashCommand
+
+
+async def get_prefix(_, message):
+    """Retrieves the prefix the bot is listening to with the message as a context."""
+    return await search_prefix(str(message.guild.id))
+
+bot = commands.Bot(command_prefix=get_prefix)
+slash = SlashCommand(bot, sync_commands=True)
+
+
+@bot.event
+async def on_ready():
+    """
+    Prints that the bot is logged on once it's logged on
+    """
+    print(f'Logged on as {bot.user.name}!')
+    await refresh_top100()
+    print('\ndone!')
+
+
+@tasks.loop(minutes=5)
+async def refresh_ranks():
+    """Refreshes the list of the top 100 players in the world"""
+    await refresh_top100()
+    print('\ndone on interval!')
+
+
+@bot.event
+async def on_guild_join(guild):
+    """Sets the default prefix to period"""
+    await replace_prefix(str(guild.id), '.')
+
+
+@bot.event
+async def on_guild_remove(guild):
+    """Removes the server from the database"""
+    await leave_guild(str(guild.id))
+
+
+# The following functions are bot commands, with each given a slash command counterpart.
+# Slash commands are not yet implemented in discord.py so the implementation for both
+# can't be put under the same function, but this will be updated once it has been.
+
+
+@bot.command(name='commands', help='Lists all the commands I know!')
+async def com(ctx):
+    """Sends out a text guide on the available commands"""
+
+    await ctx.send(f'{ctx.author.mention}, '
+                   f'these are the available commands:\n'
+                   f'`.addme [BattleTag]` :: '
+                   'adds your BattleTag to my memory!\n'
+                   f'`.hero [hero_name]` :: '
+                   'displays your competitive stats on a partiular hero\n'
+                   f'`.compare [discord_user_1] [discord_user_2] [hero_name]` '
+                   ':: compares two players\' stats on a particular hero')
+
+
+@slash.slash(name='commands', description='Lists all the commands I know!')
+async def com_slash(ctx):
+    """Sends out a text guide on the available commands.
+    Slash version.
+    """
+
+    await ctx.send(f'{ctx.author.mention}, '
+                   f'these are the available commands:\n'
+                   f'`.addme [BattleTag]` :: '
+                   'adds your BattleTag to my memory!\n'
+                   f'`.hero [hero_name]` :: '
+                   'displays your competitive stats on a partiular hero\n'
+                   f'`.compare [discord_user_1] [discord_user_2] [hero_name]` '
+                   ':: compares two players\' stats on a particular hero')
+
+
+@bot.command(name='prefix', help='[Restricted to admins] changes my prefix to whatever you want!')
+@has_permissions(administrator=True)
+async def changeprefix(ctx, new_prefix):
+    """Changes the bot prefix to the new_prefix"""
+
+    await replace_prefix(str(ctx.guild.id), new_prefix)
+    await ctx.send(f'{ctx.message.author.mention}, '
+                   f'the prefix has been changed to {new_prefix}')
+
+
+@slash.slash(name='prefix', description='Changes my prefix to whatever you want!')
+@has_permissions(administrator=True)
+async def changeprefix_slash(ctx, new_prefix):
+    """Changes the bot prefix to the new_prefix"""
+
+    await replace_prefix(str(ctx.guild.id), new_prefix)
+    await ctx.send(f'{ctx.author.mention}, '
+                   f'the prefix has been changed to {new_prefix}')
+
+
+@changeprefix.error
+async def changeprefix_error(ctx, error):
+    """Error handling for lacking admin permissions
+    when running changeprefix
+    """
+    if isinstance(error, CheckFailure):
+        msg = "You're an average joe {}".format(ctx.message.author.mention)
+        await ctx.send(msg)
+
+
+@changeprefix_slash.error
+async def changeprefix_slash_error(ctx, error):
+    """Error handling for lacking admin permissions
+    when running changeprefix_slash
+    """
+    if isinstance(error, CheckFailure):
+        msg = f"{ctx.author.mention}".format(
+        await ctx.send(msg)
+
+
+@bot.command(help='Adds the battle ID to the discord user in the database\n'
+             f'Usage:\n{get_prefix}addme battle_id \nsuch as: '
+             f'{get_prefix}addme Exor#11705')
+async def addme(ctx, *args):
+    """Adds the listed battle id to the discord user in the database"""
+
+    if len(args) != 1:
+        await ctx.send('{0.author.mention}, please use your command this way:'
+                       ' \n.addme battle_id \nsuch as: '
+                       '.addme Exor#11705'.format(ctx.message))
+        return
+
+    nonactivity = await set_user(str(ctx.guild.id), args[0],
+                                 replace_last_char(str(ctx.message.author), '#', '-'))
+    if nonactivity == 0:
+        await ctx.send('{0.author.mention}, '
+                       'the battle_id you added entered '
+                       'could not be found.'.format(ctx.message))
+        return
+    if nonactivity == 1:
+        await ctx.send('{0.author.mention}, '
+                       'the battle_id you added entered '
+                       'is set to private.'.format(ctx.message))
+        return
+    if nonactivity == 2:
+        await ctx.send('{0.author.mention}, '
+                       'your battle_id was succesfully '
+                       'added to my memory!'.format(ctx.message))
+        return
+
+
+@slash.slash(name='addme', description='Adds the battle ID to the discord user in the database')
+async def addme_slash(ctx, battle_id):
+    """Adds the listed battle id to the discord user in the database.
+    Slash version.
+    """
+
+    nonactivity = await set_user(str(ctx.guild.id), battle_id,
+                                 replace_last_char(str(ctx.author), '#', '-'))
+    if nonactivity == 0:
+        await ctx.send('{0.author.mention}, '
+                       'the battle_id you added entered '
+                       'could not be found'.format(ctx))
+        return
+    if nonactivity == 1:
+        await ctx.send('{0.author.mention}, '
+                       'the battle_id you added entered '
+                       'is set to private'.format(ctx))
+        return
+    if nonactivity == 2:
+        await ctx.send('{0.author.mention}, '
+                       'your battle_id was succesfully '
+                       'added to my memory!'.format(ctx))
+
+
+@bot.command(help='Compares the stats of the two listed players from the database.\n'
+             f'Usage:\n{get_prefix}compare user1 user2, such as: '
+             f'\n{get_prefix}compare Exor Bosco Ana')
+async def compare(ctx, *args):
+    """Compares the stats of the two listed players from the database."""
+
+    if len(args) != 3:
+        await ctx.send('{0.author.mention}, '
+                       'please use your command this way: '
+                       '\n!compare user1 user2, such as: '
+                       '\n!compare Exor Bosco Ana'.format(ctx.message))
+        return
+    try:
+        hero = args[2].lower()
+        data = await database_compare(str(ctx.guild.id), args[0], args[1], hero)
+        player_1_hashtag = replace_last_char(await user_top_match(str(ctx.guild.id),
+                                                                  args[0]), '-', '#')
+        player_2_hashtag = replace_last_char(await user_top_match(str(ctx.guild.id),
+                                                                  args[1]), '-', '#')
+        len_1 = len(player_1_hashtag)
+        space_gap = ' ' * max(0, round(1.8 * (25 - len_1)))
+        await ctx.send(f'{ctx.message.author.mention}, here is how '
+                       f'{player_1_hashtag} '
+                       f'and {player_2_hashtag}'
+                       f' match up on {hero}:\n\n'
+                       f'{player_1_hashtag}'
+                       f'{space_gap}----               '
+                       f'{player_2_hashtag}'
+                       f'\n------------------------------'
+                       f'-----------------------------------{data}')
+        return
+    except ValueError as err:
+        await ctx.send(f'{ctx.message.author.mention}, {err}')
+        return
+
+
+@slash.slash(name='compare', description='Compares the stats '
+             'of the two listed players from the database.')
+async def compare_slash(ctx, user_1, user_2, hero_name):
+    """Compares the stats of the two listed players from the database.
+    Slash version."""
+
+    try:
+        hero = hero_name.lower()
+        data = await database_compare(str(ctx.guild.id), user_1, user_2, hero)
+        player_1_hashtag = replace_last_char(await user_top_match(str(ctx.guild.id),
+                                                                  user_1), '-', '#')
+        player_2_hashtag = replace_last_char(await user_top_match(str(ctx.guild.id),
+                                                                  user_2), '-', '#')
+        len_1 = len(player_1_hashtag)
+        space_gap = ' ' * max(0, round(1.8 * (25 - len_1)))
+        await ctx.send(f'{ctx.message.author.mention}, here is how '
+                       f'{player_1_hashtag} '
+                       f'and {player_2_hashtag}'
+                       f' match up on {hero}:\n\n'
+                       f'{player_1_hashtag}'
+                       f'{space_gap}----               '
+                       f'{player_2_hashtag}'
+                       f'\n------------------------------'
+                       f'-----------------------------------{data}')
+        return
+    except ValueError as err:
+        await ctx.send(f'{ctx.author.mention}, {err}')
+        return
+
+
+@bot.command(name='hero', help='Shows your stats on a specific hero!\n'
+             f'Usage:\n{get_prefix}hero [hero_name], such as:'
+             f'\n{get_prefix}hero ana')
+async def single_hero_stats(ctx, hero_name):
+    """Shows the stats of the calling discord user on a specific hero"""
+
+    try:
+        data = await find_hero(str(ctx.guild.id), replace_last_char(
+            str(ctx.message.author), '#', '-'), hero_name.lower())
+        await ctx.send(f'{ctx.message.author.mention}, '
+                       f'here are your stats on {hero_word_match(hero_name)} per 10: {data}')
+        return
+    except ValueError:
+        await ctx.send(f'{ctx.message.author.mention}, {hero_name} is not a valid hero name!')
+        return
+    except AttributeError as err:
+        if err == 'no playtime':
+            await ctx.send(f'{ctx.message.author.mention}, '
+                           'you have no playtime on {hero_name} this season!')
+            return
+        await ctx.send(f'{ctx.message.author.mention}, you have not '
+                       'added yourself to my database yet! '
+                       'Please use the addme command first.')
+        return
+
+
+@slash.slash(name='hero', description='Shows your stats on a specific hero!')
+async def single_hero_stats_slash(ctx, hero_name):
+    """Shows the stats of the calling discord user on a specific hero.
+    Slash version
+    """
+
+    try:
+        data = await find_hero(str(ctx.guild.id), replace_last_char(
+            str(ctx.author), '#', '-'), hero_name.lower())
+        await ctx.send(f'{ctx.author.mention}, '
+                       f'here are your stats on {hero_word_match(hero_name)} per 10: {data}')
+        return
+    except ValueError as err:
+        await ctx.send(f'{ctx.author.mention}, {err}')
+        return
+    except AttributeError as err:
+        if err == 'no playtime':
+            await ctx.send(f'{ctx.author.mention}, '
+                           'you have no playtime on {hero_name} this season!')
+            return
+        await ctx.send(f'{ctx.author.mention}, you have not '
+                       'added yourself to my database yet! '
+                       'Please use the addme command first.')
+        return
+
+
+bot.run('ODk3OTA5MTcwODA2ODgyMzY1.YWchFA.0pbCHQYXEBylmoBWM_5lfh7G3fM')
